8b0d8a2fffdad33253af04668f0b4edf2f663986
Хардин Виталий <V.KHardin@1nhtk.local>
2026-06-05 08:43:55 +0700

    feat: Базовая разметка backend

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import express, { json } from 'express'
 import { initDatabase } from './db/init.js'
-// import adminRoutes from './modules/adminRoutes'
-// import authRoutes from './modules/authRoutes'
-// import bookingRoutes from './modules/bookingRoutes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-/* Middleware */
 app.use(cors())
 app.use(json())
 
-/* Маршруты API */
-// app.use('/api/auth', authRoutes)
-// app.use('/api/bookings', bookingRoutes)
-// app.use('/api/admin', adminRoutes)
-
-/* Запуск сервера после инициализации БД */
+// Запуск сервера
 const start = async () => {
 	await initDatabase()
 
